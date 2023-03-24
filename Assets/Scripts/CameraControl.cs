71c9033261ba99fdb4bf2ed23ccc46f89f2bdf36
ktaylor120 <124301504+ktaylor120@users.noreply.github.com>
2023-03-23 20:29:04 -0700

    camera control

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
+    public Vector2 turn;
+    public float speed = 1;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        turn.x += Input.GetAxis("Mouse X");
+        turn.y += Input.GetAxis("Mouse Y");
+        transform.localRotation = Quaternion.Euler(-turn.y, turn.x, 0);
+
+        transform.Translate(Input.GetAxisRaw("Horizontal") * speed, 0, Input.GetAxisRaw("Vertical") * speed);
     }
 }
